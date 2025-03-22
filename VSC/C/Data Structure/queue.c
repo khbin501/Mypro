125d66226a971cd93dcd196db2c7e992d609e564
khbin501 <khbin501@gmail.com>
2025-03-22 23:23:05 +0900

    20250322

--- VSC/C/Data Structure/queue.c
+++ VSC/C/Data Structure/queue.c
@@ -1,24 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 4
 
-int arr[MAX]={0}, front=-1, back=-1;
+int arr[MAX] = {0}, front = -1, back = -1;
 
-void inQue(int arr[],int data){
-    if(back > MAX-1){
+void inQue(int arr[], int data) {
+    if (back > MAX - 1) {
         printf("queue is full");
         return;
     }
 
-    if(front == -1){
+    if (front == -1) {
         front = 0;
     }
     arr[++back] = data;
 }
 
-int deQue(int arr[]){
-    if(back < front || back == -1){
+int deQue(int arr[]) {
+    if (back < front || back == -1) {
         printf("queue is empty");
         exit(1);
     }
@@ -28,21 +28,17 @@ int deQue(int arr[]){
     return result;
 }
 
-void printQue(int arr[]){
-    for (int i = 0; i < MAX; i++)
-    {
+void printQue(int arr[]) {
+    for (int i = 0; i < MAX; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-
-
-int main(){
-
-    inQue(arr,1);
-    inQue(arr,2);
-    inQue(arr,3);
+int main() {
+    inQue(arr, 1);
+    inQue(arr, 2);
+    inQue(arr, 3);
 
     printQue(arr);
 
